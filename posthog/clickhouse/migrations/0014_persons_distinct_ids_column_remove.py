8ef7e3bfa4b2c79ed50fef3a43fd789d74e97c14
mudux <mohamud.a.a@gmail.com>
2025-07-07 20:51:17 +0300

    Fix migration scripts to use "DROP TABLE IF EXISTS" for safer table removal

--- posthog/clickhouse/migrations/0014_persons_distinct_ids_column_remove.py
+++ posthog/clickhouse/migrations/0014_persons_distinct_ids_column_remove.py
@@ -3,8 +3,8 @@ from posthog.models.person.sql import KAFKA_PERSONS_TABLE_SQL, PERSONS_TABLE_MV_
 from posthog.settings import CLICKHOUSE_CLUSTER
 
 operations = [
-    run_sql_with_exceptions(f"DROP TABLE person_mv ON CLUSTER '{CLICKHOUSE_CLUSTER}'"),
-    run_sql_with_exceptions(f"DROP TABLE kafka_person ON CLUSTER '{CLICKHOUSE_CLUSTER}'"),
+    run_sql_with_exceptions(f"DROP TABLE IF EXISTS person_mv ON CLUSTER '{CLICKHOUSE_CLUSTER}'"),
+    run_sql_with_exceptions(f"DROP TABLE IF EXISTS kafka_person ON CLUSTER '{CLICKHOUSE_CLUSTER}'"),
     run_sql_with_exceptions(f"ALTER TABLE person ON CLUSTER '{CLICKHOUSE_CLUSTER}' DROP COLUMN IF EXISTS distinct_ids"),
     run_sql_with_exceptions(KAFKA_PERSONS_TABLE_SQL()),
     run_sql_with_exceptions(PERSONS_TABLE_MV_SQL),
